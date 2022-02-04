29c155a0e2a6cb47666230e6dbf5f80676afdb79
agila <agiladhanasekar@gmail.com>
2022-02-04 11:43:31 +0530

    third message

--- src/main/java/com/flightapp/gateway/ApiGatewayApplication.java
+++ src/main/java/com/flightapp/gateway/ApiGatewayApplication.java
@@ -1,8 +1,11 @@
 package com.flightapp.gateway;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +14,7 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+	
+	
 
 }
